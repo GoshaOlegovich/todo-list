faca1bedfc9f4b1d28dfa4cc06ec7fc5316dabc2
GoshaOlegovich <web.ingvarr@gmail.com>
2022-06-14 11:02:15 +0300

    o

--- src/script/script.js
+++ src/script/script.js
@@ -1,9 +1,7 @@
 // Modal 
 const btnNewTask = document.querySelector('.new-task-btn'),
-    modal = document.querySelector('.modal');
-
-// Modal form
-const form = document.querySelector('.modal__form'),
+    modal = document.querySelector('.modal'),
+    form = document.querySelector('.modal__form'),
     input = document.querySelector('.modal__input--title'),
     inputDesc = document.querySelector('.modal__input--textarea'),
     checkBoxesContainer = document.querySelector('.modal__checkboxes'),
@@ -11,18 +9,19 @@ const form = document.querySelector('.modal__form'),
     checkBoxes = document.querySelectorAll('.checkbox-proprity');
 
 
-const limitClass = document.querySelectorAll('.limit');
+const limitClass = document.querySelectorAll('.limit'); // validation
 
 
 
 // Task list
-const taskStorage = [];
+
+const taskStorage = []; 
 
 const tasksList = document.querySelector('.task__list'),
-    taskArchive = document.querySelector('.task-archive__container');
+      taskArchive = document.querySelector('.task-archive__container');
 
-// Open Modal
-btnNewTask.addEventListener('click', () => modal.style.display = 'block');
+
+
 
 
 const taskBtnDone = document.querySelectorAll('.task__btn')
@@ -43,23 +42,8 @@ const getCheckBoxValue = () => {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const todo = () => {
     textLength.innerHTML = 0
-
 
     inputDesc.addEventListener('keydown', symbolLengthFunc = () => {
         let symbolLength = inputDesc.value.length;
@@ -78,99 +62,103 @@ const todo = () => {
         return symbolLength;
     })
 
-    //Form 
-
-
-
     form.addEventListener('submit', (e) => {
         e.preventDefault();
 
         let currentPriority = getCheckBoxValue();
         let symbolLengthOut = symbolLengthFunc();
 
-
-        const FormValidation = () => {
-            if (input.value === '') {
-
-                input.classList.add('modal__input--error');
-            } else if (currentPriority === undefined) {
-
-                checkBoxesContainer.classList.add('modal__checkboxes-- error');
-            } else if (symbolLengthOut > 120) {
-
-            } else {
-                textLength.innerHTML = 0 
-                input.classList.remove('modal__input--error');
-                checkBoxesContainer.classList.remove('modal__checkboxes-- error');
-                modal.style.display = 'none';
-
-                // Create task El
-                let task = input.value;
-                let taskDesc = inputDesc.value;
-
-                class CreateTask {
-                    render(val) {
-                        const el = document.createElement('li');
-                        el.className = 'task__item';
-                        el.setAttribute('data-index', val)
-                        el.innerHTML = `
-                <div class="task__head">
-                <input class="task__btn" type="radio" id="done-task-${val}"> 
-                <label class="task__btn--label" for="done-task-${val}" data-index=${val}></label>
-                <h4 class="task__title">${task}</h4>
-                <span class="priority priority--${currentPriority}">${currentPriority}</span>
-                </div>
-                <p class="task__desc">${taskDesc}</p>`;
-                        taskStorage.push(el);
-                        tasksList.appendChild(el);
-
-
-                    }
-                }
-                let index = 0;
-                const elEnumeratio = () => {
-                    for (i = 0; i < taskStorage.length; i++) {
-                        index++;
-                    }
-
-                }
-                elEnumeratio();
-                const createTask = new CreateTask();
-                createTask.render(index);
-                form.reset();
-            }
-
-
-        }
-
-        FormValidation();
-
-        //  task done
-
-       
-
-
+        FormValidation(currentPriority, symbolLengthOut);
 
     })
-
 
 }
 
 
-todo()
+
+//Validation 
+
+const FormValidation = (currentPriority, symbolLengthOut) => {
+    if (input.value === '') {
+
+        input.classList.add('modal__input--error');
+    } else if (currentPriority === undefined) {
+
+        checkBoxesContainer.classList.add('modal__checkboxes-- error');
+    } else if (symbolLengthOut > 120) {
+
+    } else {
+        textLength.innerHTML = 0
+        input.classList.remove('modal__input--error');
+        checkBoxesContainer.classList.remove('modal__checkboxes-- error');
+        modal.style.display = 'none';
+
+       
+        createTask(currentPriority)
+       
+    }
+    form.reset();
+
+}
 
 
+//Create task 
+
+const createTask = (currentPriority) => { 
+    let task = input.value;
+    let taskDesc = inputDesc.value;
+    class CreateTask {
+        render(val) {
+            const el = document.createElement('li');
+            el.className = 'task__item';
+            el.setAttribute('data-index', val)
+            el.innerHTML = `
+        <div class="task__head">
+        <input class="task__btn" type="radio" id="done-task-${val}"> 
+        <label class="task__btn--label" for="done-task-${val}" data-index=${val}></label>
+        <h4 class="task__title">${task}</h4>
+        <span class="priority priority--${currentPriority}">${currentPriority}</span>
+        </div>
+        <p class="task__desc">${taskDesc}</p>`;
+            taskStorage.push(el);
+            tasksList.appendChild(el);
 
 
-    tasksList.addEventListener('click', (e) => {
-        let currentTask = e.target;
-        let currentTaskIndex = currentTask.dataset.index;
-        console.log(currentTask + currentTaskIndex);
-        for (i = 0; i < taskStorage.length; i++) {
-            setTimeout(() => {
-                taskStorage[currentTaskIndex].remove();
-            }, 500);
-           
         }
-        
-    })
+    }
+    let index = 0;
+    const elEnumeratio = () => {
+        for (i = 0; i < taskStorage.length; i++) {
+            index++;
+        }
+    
+    }
+    elEnumeratio();
+    const createTask = new CreateTask();
+    createTask.render(index);
+   
+}
+
+
+//Done task
+
+tasksList.addEventListener('click', (e) => {
+    let currentTask = e.target;
+    let currentTaskIndex = currentTask.dataset.index;
+    console.log(currentTask + currentTaskIndex);
+    for (i = 0; i < taskStorage.length; i++) {
+        setTimeout(() => {
+            taskStorage[currentTaskIndex].remove();
+        }, 500);
+
+    }
+
+})
+
+
+
+// Open Modal
+btnNewTask.addEventListener('click', () => modal.style.display = 'block');
+
+
+todo()
